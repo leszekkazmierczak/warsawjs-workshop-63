3b9f07f3c36f14a240bf401b2376fceb10e1ab72
leszekkazmierczak <leszek@o-it.pl>
2022-06-19 09:54:19 +0000

    Prettier setup and compontents styling

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-import Text from './components/Text';
+import "./App.css";
+import Text from "./components/Text";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-
         <Text defaultParameter={2} />
-
         <Text defaultParameter={25} />
-
       </header>
     </div>
   );
--- src/components/Text.js
+++ src/components/Text.js
@@ -1,23 +1,22 @@
 import { useState } from "react";
-import '../Custom.css';
+import "../Custom.css";
 
-const Text = ({ text = "Counter",  defaultParameter = 0}) => {
+const Text = ({ text = "Counter", defaultParameter = 0 }) => {
+  const [counter, setCounter] = useState(defaultParameter);
 
-    const [counter, setCounter] = useState(defaultParameter);
+  const onChange = (increment) => {
+    setCounter(counter + increment);
+  };
 
-    const onChange = (increment) => {
-        setCounter(counter + increment);
-    }
-
-    return (
-        <div>
-            <p>{text}</p>
-            <h2>{counter}</h2>
-            <button onClick={() => onChange(1)}>+</button>
-            <button onClick={() => onChange(-1)}>-</button>
-            <button onClick={() => onChange(5)}> +5</button>
-        </div>
-    )
-}
+  return (
+    <div>
+      <p>{text}</p>
+      <h2>{counter}</h2>
+      <button onClick={() => onChange(1)}>+</button>
+      <button onClick={() => onChange(-1)}>-</button>
+      <button onClick={() => onChange(5)}> +5</button>
+    </div>
+  );
+};
 
 export default Text;
