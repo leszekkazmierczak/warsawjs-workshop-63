88031f055ca973ca8ec21bc20d4ddb3760bcb65c
leszekkazmierczak <leszek@o-it.pl>
2022-06-19 12:05:44 +0000

    First working version

--- src/App.js
+++ src/App.js
@@ -1,12 +1,57 @@
+import { useEffect, useState } from "react";
 import "./App.css";
-import Text from "./components/Text";
+import Capture from "./components/Capture";
 
 function App() {
+  const [file, setFile] = useState();
+  const [preview, setPreview] = useState();
+  const [text1, setText1] = useState("Text1");
+  const [text2, setText2] = useState("Text2");
+
+  const onFileInputChange = (event) => {
+    setFile(event.target.files[0]);
+  };
+
+  const onTextTopInputChange = (event) => {
+    setText1(event.target.value);
+  };
+
+  const onTextBottomInputChange = (event) => {
+    setText2(event.target.value);
+  };
+
+  useEffect(() => {
+    if (!file) {
+      setPreview(undefined);
+      return;
+    }
+
+    const objectUrl = URL.createObjectURL(file);
+    setPreview(objectUrl);
+    // free memory when ever this component is unmounted
+    return () => URL.revokeObjectURL(objectUrl);
+  }, [file]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <Text defaultParameter={2} />
-        <Text defaultParameter={25} />
+        {preview ? (
+          <Capture img={preview} textTop={text1} textBottom={text2} />
+        ) : (
+          <h2>Import an image first</h2>
+        )}
+        <input type={"file"} onChange={onFileInputChange} />
+
+        <input
+          type={"text1"}
+          placeholder="Input top text"
+          onChange={onTextTopInputChange}
+        />
+        <input
+          type={"text2"}
+          placeholder="Input bottom text"
+          onChange={onTextBottomInputChange}
+        />
       </header>
     </div>
   );
---
+++ src/components/Capture.js
@@ -0,0 +1,54 @@
+import React, { useCallback, useRef } from "react";
+import html2canvas from "html2canvas";
+import "../Custom.css";
+
+const exportAsImage = async (el, imageFileName) => {
+  const canvas = await html2canvas(el);
+  const image = canvas.toDataURL("image/png", 1.0);
+  downloadImage(image, imageFileName);
+};
+const downloadImage = (blob, fileName) => {
+  const fakeLink = window.document.createElement("a");
+  fakeLink.style = "display:none;";
+  fakeLink.download = fileName;
+
+  fakeLink.href = blob;
+
+  document.body.appendChild(fakeLink);
+  fakeLink.click();
+  document.body.removeChild(fakeLink);
+
+  fakeLink.remove();
+};
+
+const Capture = ({ img, textTop, textBottom }) => {
+  const ref = useRef(null);
+
+  return (
+    <>
+      <div
+        ref={ref}
+        style={{
+          backgroundImage: `url(${img})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          position: "relative",
+          width: 450,
+          height: 800,
+          backgroundRepeat: "no-repeat",
+          display: "flex",
+          justifyContent: "space-between",
+          flexDirection: "column",
+        }}
+      >
+        <p className="memeText">{textTop}</p>
+        <p className="memeText">{textBottom}</p>
+      </div>
+      <button onClick={() => exportAsImage(ref.current, "test")}>
+        Download
+      </button>
+    </>
+  );
+};
+
+export default Capture;
